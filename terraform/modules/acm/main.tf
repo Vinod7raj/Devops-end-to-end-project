310cec27745e6eaa42b02e05c454904e250b696d
Ubuntu <ubuntu@ip-172-31-38-173.ec2.internal>
2026-09-24 06:14:45 +0000

    minor chnage

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -9,25 +9,25 @@ resource "aws_acm_certificate" "pollingapp_cert" {
   }
 }
 
-resource "aws_route53_record" "pollingapp_cert_validation" {
- for_each = {
-   for dvo in aws_acm_certificate.pollingapp_cert.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-type   = dvo.resource_record_type
-        record = dvo.resource_record_value
-       }
-     }
+#resource "aws_route53_record" "pollingapp_cert_validation" {
+# for_each = {
+#   for dvo in aws_acm_certificate.pollingapp_cert.domain_validation_options : dvo.domain_name => {
+#    name   = dvo.resource_record_name
+#type   = dvo.resource_record_type
+#        record = dvo.resource_record_value
+#       }
+#     }
+#
+#      zone_id = var.pollingapp_zone_id
+#     name    = each.value.name
+#     type    = each.value.type
+#     records = [each.value.record]
+#     ttl     = 60
+# }
 
-      zone_id = var.pollingapp_zone_id
-     name    = each.value.name
-     type    = each.value.type
-     records = [each.value.record]
-     ttl     = 60
- }
-
-  resource "aws_acm_certificate_validation" "pollingapp_cert_validation" {
-  certificate_arn         = aws_acm_certificate.pollingapp_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.pollingapp_cert_validation : record.fqdn]
-  }
+#  resource "aws_acm_certificate_validation" "pollingapp_cert_validation" {
+#  certificate_arn         = aws_acm_certificate.pollingapp_cert.arn
+#  validation_record_fqdns = [for record in aws_route53_record.pollingapp_cert_validation : record.fqdn]
+#  }
 
 
